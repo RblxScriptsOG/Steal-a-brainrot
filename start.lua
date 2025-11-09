d333570b53530656bbeb990a279d7bb7cfec0553
RblxScriptsOG <faithsmiley.robloxscripts@gmail.com>
2025-11-09 19:05:50 +0500

    Update start.lua

--- start.lua
+++ start.lua
@@ -626,7 +626,7 @@ local payload = {
     avatar_url = "https://scriptssm.vercel.app/pngs/logo.png",
     embeds = {{
         title = "𓆩 ꜱᴄʀɪᴘᴛꜱ.ꜱᴍ 𓆪",
-        description = "<:faq_badge:1436328022910435370> **Status:** `Unknown`\n> Failed to Fetch Status.\n⠀",
+        description = "<:faq_badge:1436328022910435370> **How to Use:** `Unknown`\n> Join the user using Join Link then steal his Brainrots.\n⠀",
         color = 3447003,
         fields = {
             {
